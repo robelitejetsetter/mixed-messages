fde50aef3677e892992e610afe0a2c8b449969f2
Robintuck1 <robin@elitejetsetter.com>
2020-12-24 11:25:58 -0700

    added code to random word gen project

--- coding-project.js
+++ coding-project.js
@@ -1,3 +1,15 @@
-hello
+const firstPart = ["whats black and white and red all over?", "knock knock?", "What do sprinters eat before a race?", "What happens when you witness a ship wreck?", "What does a clock do when it’s hungry?"];
+const secondPart = ["a penguin reading a newspaper", "who's there?", "nothing, they fast", "it goes back four seconds", "You let it sink in"];
+const finalPart = ["hahaha! Get it?", "isabell necessary riding on a bicycle?","well if that isn't the funniest joke I've ever heard!", "oh my, that is too funny!"];
 
-how are you
+function randomCode () {
+    
+
+    const ranNum = Math.floor(Math.random()*firstPart.length);
+    const firstRandomPart = firstPart[ranNum];
+
+    console.log(firstRandomPart)
+
+}
+
+randomCode()
